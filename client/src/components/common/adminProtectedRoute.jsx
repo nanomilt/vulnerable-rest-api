d709909873adab6badabfc11a44b2853aeba0858
MiltosNanos <nanomilt@ece.auth.gr>
2025-04-11 13:13:16 +0300

    claude-default_name-vulnerable-rest-api Fixing code violations

--- client/src/components/common/adminProtectedRoute.jsx
+++ client/src/components/common/adminProtectedRoute.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { Route, Redirect } from 'react-router-dom';
 import auth from '../../services/authService';
 
-const AdminProtectedRoute = ({path, component:Component, render, ...rest }) => {
+const AdminProtectedRoute = ({path, component: Component, render, ...rest }) => {
     return (
         <Route
         {...rest}
