898f6d6dc241337b0dcddcc7cf6ea6d6871cf895
MiltosNanos <nanomilt@ece.auth.gr>
2025-04-16 13:53:27 +0300

    claude-vulnerable-rest-api Fixing Code-Violations

--- server/routes/authors.js
+++ server/routes/authors.js
@@ -20,21 +20,23 @@ router.post('/', auth, async(req,res)=>{
     if(author) return res.status(400).send('Author is Already Existed!');
 
     author = new Author(req.body);
-    author.save();
+    await author.save();
     res.status(201).send(author);
 })
 
 router.put('/:id', auth, async(req,res)=>{
-    await Author.findByIdAndUpdate({_id: req.params.id}, {
+    const updatedAuthor = await Author.findByIdAndUpdate({_id: req.params.id}, {
         $set: {
             name: req.body.name,
             email: req.body.email,
             about: req.body.about,
             job: req.body.job
         }
-    })
+    }, {new: true});
 
-    res.send('Updated Successfully');
+    if(!updatedAuthor) return res.status(404).send('The author with the given ID was not found');
+
+    res.send(updatedAuthor);
 })
 
 router.delete('/:id', auth, async(req,res)=>{
@@ -45,4 +47,3 @@ router.delete('/:id', auth, async(req,res)=>{
 })
 
 module.exports = router;
-
