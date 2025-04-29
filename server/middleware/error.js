471eacb77251ff9683da382883c7bd366f6c3610
MiltosNanos <nanomilt@ece.auth.gr>
2025-04-29 12:07:19 +0300

    claude-vulnerable-rest-api Fixing Code-Violations

--- server/middleware/error.js
+++ server/middleware/error.js
@@ -1,6 +1,7 @@
 const winston = require('winston');
 
 module.exports = function(err, req, res, next){
+    const _ = next; // Unused variable declared to avoid linting error
     winston.error(err.message, err);
     res.status(500).send('Server Error');
 }
