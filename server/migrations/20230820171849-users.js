de7ef757db032ac9b6f10949ba90c26787ab9744
MiltosNanos <nanomilt@ece.auth.gr>
2025-05-06 16:34:24 +0300

    claude-vulnerable-rest-api Fixing Code-Violations

--- server/migrations/20230820171849-users.js
+++ server/migrations/20230820171849-users.js
@@ -12,8 +12,7 @@ module.exports = {
   },
 
   async down(db, client) {
-    // TODO write the statements to rollback your migration (if possible)
-    // Example:
-    // await db.collection('albums').updateOne({artist: 'The Beatles'}, {$set: {blacklisted: false}});
+    // Statements to rollback the migration
+    await db.collection("users").deleteMany({ "username": "admin" });
   }
 };
