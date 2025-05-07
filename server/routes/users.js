a15ef144cacbfb9a03f975ea8b6712fa1dd59961
MiltosNanos <nanomilt@ece.auth.gr>
2025-05-07 15:37:02 +0300

    claude-vulnerable-rest-api Fixing Code-Violations

--- server/routes/users.js
+++ server/routes/users.js
@@ -45,9 +45,9 @@ router.post('/', async (req, res)=>{
 
 router.put('/:id', [auth, validateObjectId], async(req, res)=>{
 
-    let user = await User.findOne({_id: req.params.id});
+    const user = await User.findOne({_id: req.params.id});
 
-    var domain;
+    let domain;
     await needle('get', req.body.url)
         .then(function(resp) { domain =  resp.body; })
         .catch(function(err) { return; })
@@ -68,7 +68,7 @@ router.put('/:id', [auth, validateObjectId], async(req, res)=>{
 
 router.post('/otp', async(req,res)=>{
     const user = await User.findOne({username: req.body.username});
-    if(!user.email) return res.status(404).send('User does not exist!');
+    if(!user) return res.status(404).send('User does not exist!');
 
     // generate the token
     const generatedOTP = Math.floor(Math.random() * 9000 + 1000);
@@ -116,4 +116,3 @@ router.delete('/:id', [auth, validateObjectId], async(req,res)=>{
 })
 
 module.exports = router;
-
